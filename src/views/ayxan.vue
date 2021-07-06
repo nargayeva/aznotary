3d8cfad7ce58190ead9fe7ccf0943414ae4b264e
Narmina Aghayeva <nargayeva@gmail.com>
2021-07-06 16:36:29 +0400

    texts updated

--- src/views/ayxan.vue
+++ src/views/ayxan.vue
@@ -6,7 +6,7 @@
       image="ayxan.jpg"
       body="
       <p>Məmmədli Ayxan Qüdrət oğlu, 21.09.1996-cı ildə Bakı şəhərində anadan olmuşdur. Orta təhsilini 2002-2013-cü illərdə Bakı şəhəri, Yasamal rayonu 153 saylı orta məktəbdə almışdır. Ali təhsilini Milli Aviasiya Akademiyasının Uçuş Mühəndisliyi ixtisası üzrə almışdır. 2018-2019-cu illərdə həqiqi hərbi xidmətdə olmuşdur. 03.07.2020-ci il tarixdən Bakı şəhəri, 52 saylı notariat ofisində könüllü kimi fəaliyyətə başlamışdır. 28.09.2020-ci il tarixdə Yasamal rayon hərbi komissarlığı tərəfindən Silahlı Qüvvələrin təliminə cəlb olunmuş, 29.09.2021-ci il tarixdə Ali Baş Komandanın əmrinə əsasən işğal olunmuş torpaqlarımızın azad olunması uğrunda döyüşlərə qatılmışdır. Cəbrayıl, Füzuli, Xocavənd rayonlarının azadlığı uğrunda gedən döyüşlərdə iştirak etmişdir. </p>
-    <p>Azərbaycan Respublikasının ədliyyə naziri I dərəcəli dövlət ədliyyə müşaviri Fikrət Məmmədovun əmrinə əsasən Vətən müharibəsində iştirak edərək fədakarlıq göstərdiyinə görə Fəxri fərmanla təltif edilmişdir.</p>"
+    <p>Azərbaycan Respublikası Prezidenti, Silahlı Qüvvələrin Ali Baş Komandanı İlham Əliyevin sərəncamına əsasən “Laçının azad olunmasına görə” medalı  ilə, o cümlədən  Azərbaycan Respublikasının ədliyyə naziri I dərəcəli dövlət ədliyyə müşaviri Fikrət Məmmədovun əmrinə əsasən Vətən müharibəsində iştirak edərək fədakarlıq göstərdiyinə görə Fəxri fərmanla təltif edilmişdir.</p>"
       next="../../views/nofer"
       previous="../../views/shamxal"
     />
